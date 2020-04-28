074d77726ef37e565c7078e69e6bff983c11730c
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-04-28 16:35:24 -0400

    styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,16 +2,19 @@ import React from "react";
 // import { Link } from "react-router-dom";
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-success">
+    <nav className="navbar navbar-expand-lg navbar-light bg-success collapse navbar-collapse">
       <a className="navbar-brand" href="/">
         <h3>Good Wine and Fine Spirits</h3>
       </a>
       <br />
-      <a className="navbar-brand" href="/create">
+      <a className="navbar-brand " href="/create">
         <h6>Create a new entry</h6>
+        <span className="sr-only">(current)</span>
       </a>
       <a className="navbar-brand" href="/items">
-        <h6>Inventory List</h6>
+        <h6>
+          Inventory List<span className="sr-only">(current)</span>
+        </h6>
       </a>
 
       {/* <Link to="/create">Create</Link> */}
